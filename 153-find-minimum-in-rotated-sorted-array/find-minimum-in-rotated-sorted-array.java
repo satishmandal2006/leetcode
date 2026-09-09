3e720163a7ed7849da8aaaeb3229344969304d0c
Satish Mandal <satishmandal113@gmail.com>
2026-09-09 20:30:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (73.15%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -15,3 +15,32 @@
         return nums[low];
     }
 }
+
+// class Solution {
+//     public int findMin(int[] nums) {
+
+//         int low = 0;
+//         int high = nums.length - 1;
+//         int min = Integer.MAX_VALUE;
+
+//         while (low <= high) {
+
+//             int mid = low + (high - low) / 2;
+
+//             // Left half is sorted
+//             if (nums[low] <= nums[mid]) {
+
+//                 min = Math.min(min, nums[low]);
+//                 low = mid + 1;
+
+//             } else {
+
+//                 // Minimum is in the left unsorted half
+//                 min = Math.min(min, nums[mid]);
+//                 high = mid - 1;
+//             }
+//         }
+
+//         return min;
+//     }
+// }
